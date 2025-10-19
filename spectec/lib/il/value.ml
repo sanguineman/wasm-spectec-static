e19444e4968360db912e03b6f6d056c90e6d7d1f
KunJeong <kunjeong99@naver.com>
2025-10-20 01:39:52 +0900

    refactor: rollback value.fresh, changing module structure first

--- spectec/lib/il/value.ml
+++ spectec/lib/il/value.ml
@@ -1,11 +1,6 @@
 open Ast
 open Util.Source
 
-(* effect for value creation *)
-
-type _ Effect.t += ValueCreated : value -> unit Effect.t
-type _ Effect.t += FreshVid : (unit -> vid) Effect.t
-
 type t = value
 
 (* Ticker for node identifier tracking *)
@@ -13,28 +8,18 @@ type t = value
 let compare = compare
 let to_string t = Print.string_of_value t
 let eq (value_l : t) (value_r : t) : bool = compare value_l value_r = 0
+let tick = ref 0
+let refresh () = tick := 0
+let fresh () =
+  let id = !tick in
+  tick := id + 1;
+  id
 
 let with_fresh_val (typ : typ') : vnote =
-  let vid = Effect.perform FreshVid () in
+  let vid = fresh () in
   { vid; typ }
 
-let with_typ (typ : typ') (v : value') : t = 
-  let value = v $$$ with_fresh_val typ in
-  Effect.perform (ValueCreated value);
-  value
-
-let bool (b : bool) : t = BoolV b |> with_typ BoolT
-let nat (i : Bigint.t) : t = NumV (`Nat i) |> with_typ (NumT `NatT)
-let int (i : Bigint.t) : t = NumV (`Int i) |> with_typ (NumT `IntT)
-let text (s : string) : t = TextV s |> with_typ TextT
-
-let tuple (vs : t list) : t =
-  let typs = List.map (fun v -> v.note.typ $ no_region) vs in
-  TupleV vs |> with_typ (TupleT typs)
-let opt (typ : typ') (v : t option) : t =
-  OptV v |> with_typ (IterT (typ $ no_region, Opt))
-let list (typ : typ') (vs : t list) : t =
-  ListV vs |> with_typ (IterT (typ $ no_region, List))
+let with_typ (typ : typ') (v : value') : t = v $$$ with_fresh_val typ
 
 let get_bool (value : t) =
   match value.it with BoolV b -> b | _ -> failwith "get_bool"
@@ -54,3 +39,15 @@ let get_opt (value : t) =
 let get_struct (value : t) =
   match value.it with StructV fields -> fields | _ -> failwith "get_struct"
 
+let bool (b : bool) : t = BoolV b |> with_typ BoolT
+let nat (i : Bigint.t) : t = NumV (`Nat i) |> with_typ (NumT `NatT)
+let int (i : Bigint.t) : t = NumV (`Int i) |> with_typ (NumT `IntT)
+let text (s : string) : t = TextV s |> with_typ TextT
+
+let tuple (vs : t list) : t =
+  let typs = List.map (fun v -> v.note.typ $ no_region) vs in
+  TupleV vs |> with_typ (TupleT typs)
+let opt (typ : typ') (v : t option) : t =
+  OptV v |> with_typ (IterT (typ $ no_region, Opt))
+let list (typ : typ') (vs : t list) : t =
+  ListV vs |> with_typ (IterT (typ $ no_region, List))
